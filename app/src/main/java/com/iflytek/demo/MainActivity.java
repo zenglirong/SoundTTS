b6966c118931a77437a81b8974a3a769e8018e40
lrzeng <lrzeng@iflytek.com>
2022-12-29 13:59:05 +0800

    增加语音

--- app/src/main/java/com/iflytek/demo/MainActivity.java
+++ app/src/main/java/com/iflytek/demo/MainActivity.java
@@ -51,7 +51,6 @@ public class MainActivity extends AppCompatActivity {
 //                MediaManager mediaManager = new MediaManager(getApplicationContext(), R.raw.sound);
 //            }).start();
         });
-
     }
 
 
